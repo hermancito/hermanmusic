7554db14af062e6e8bdf15b4361413b28a6412e2
hermancito <javierhernandezcollado@gmail.com>
2026-04-02 07:35:07 +0000

    cargando plugins

--- music5/vendor/cakephp-plugins.php
+++ music5/vendor/cakephp-plugins.php
@@ -4,6 +4,9 @@ $baseDir = dirname(dirname(__FILE__));
 return [
     'plugins' => [
         'Authentication' => $baseDir . '/vendor/cakephp/authentication/',
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'Cake/TwigView' => $baseDir . '/vendor/cakephp/twig-view/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
         'Josegonzalez/Upload' => $baseDir . '/vendor/josegonzalez/cakephp-upload/',
         'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
     ],
